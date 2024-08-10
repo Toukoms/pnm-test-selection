1956c590a95f355a79b4ce94f9fed480162db80e
Toukom's <tokiniaina090605@gmail.com>
2024-08-10 16:29:12 +0300

    feat: redirect to home page if clicking on the clear-all button.

--- public/scripts/clear-all.js
+++ public/scripts/clear-all.js
@@ -3,4 +3,5 @@ const clearAllBtn = document.getElementById('clear-all');
 clearAllBtn.addEventListener('click', () => {
   localStorage.clear();
   window.location.reload();
+  window.location.assign('/index.html');
 });
